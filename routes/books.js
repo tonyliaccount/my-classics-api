f10b6bd5db82c8614da150baecbe289f0c93ac25
tonyliaccount@github.com <tony.li.email@gmail.com>
2022-08-27 03:03:59 -0700

    Last changes prior to deploy

--- routes/books.js
+++ routes/books.js
@@ -6,29 +6,40 @@ const axios = require('axios').default;
 const storage = require("../utils/storage-wrapper.js"); 
 const utils = require("./../utils/utils.js");
 
+const resultLookup = {};
+
 //get list of books based on specific queries
 router.get('/', async (req, res) => {
 
     const queryString = req.query ? new URLSearchParams(req.query).toString() : '';
 
-    const gutendexResponse = await axios.get(`https://gutendex.com/books?${queryString}`);
+    // Cache the response as gutendex is pretty slow.
+    let gutendexData = null;
+    if (queryString in resultLookup === false) {
+        console.log(`${queryString} - New result required.`)
 
-    // Santize it first - no point in returning a book if it doesn't have an ebook associated with it
+        const gutendexResponse = await axios.get(`https://gutendex.com/books?${queryString}`);
+        resultLookup[queryString] = gutendexResponse.data;
 
-    const santizedResponse = gutendexResponse.data;
+        // Santize it first - no point in returning a book if it doesn't have an ebook associated with it
+        const santizedResponse = resultLookup[queryString];
 
-    santizedResponse.next = santizedResponse.next !== null ? santizedResponse.next.substring(santizedResponse.next.indexOf('books') + 6) : null;
-    santizedResponse.previous = santizedResponse.previous !== null ? santizedResponse.previous.substring(santizedResponse.previous.indexOf('books') + 6): null;
-    
-    const prevParams = new URLSearchParams(santizedResponse.previous);
-    santizedResponse.page = prevParams.get('page') ? parseInt(prevParams.get('page')) + 1 : 1;
-    
-    santizedResponse.results = santizedResponse.results.filter( (book) => {
-        return 'application/epub+zip' in book.formats;
-    });
+        santizedResponse.next = santizedResponse.next !== null ? santizedResponse.next.substring(santizedResponse.next.indexOf('books') + 6) : null;
+        santizedResponse.previous = santizedResponse.previous !== null ? santizedResponse.previous.substring(santizedResponse.previous.indexOf('books') + 6): null;
+        
+        if (!req.query.page) {
+            santizedResponse.page = 1;
+        } else {
+            santizedResponse.page = parseInt(req.query.page);
+        }
+        
+        santizedResponse.results = santizedResponse.results.filter( (book) => {
+            return 'application/epub+zip' in book.formats;
+        });
+    }
 
     res.status(200);
-    res.send(santizedResponse);
+    res.send(resultLookup[queryString]);
 });
 
 //get information regarding a particular book, and make its epub file avaliable
